d21e6035939bee3ece9a136210bff6eecdb08c3f
maneesha_lakshani <maneeshalakshani9@gmail.com>
2022-01-30 06:10:31 +0530

    Create scripts.js file created contact popup modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-function openPopup() {
-    document.getElementById("popup").style.display = "block";
+
+function popupFunc(){
+    document.getElementById('modal-id').style.display = 'flex';
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('contact-button-id').style.display = 'none';
+}
+
+
+function closePopup(){
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('modal-id').style.display = 'none';
+    document.getElementById('contact-button-id').style.display = 'block';
 }
